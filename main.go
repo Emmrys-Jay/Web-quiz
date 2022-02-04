e28ae90eb29d2e5df5a4b7df073778a4d7c499bb
Emmrys-Jay <jonathanemma121@gmail.com>
2022-02-04 03:36:24 +0100

    I created the .go file and the .gohtml template file with no errors for a single question.
    Bu the program contains bugs because its not yet able to display a result after selecting an answer and submitting it.

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type myHandler int
+
+var tpl *template.Template
+
+var testFunc = template.FuncMap{
+	"a1": answer1,
+}
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(testFunc).ParseFiles("index.gohtml"))
+}
+
+func (m myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", r.PostForm)
+}
+
+func main() {
+	var myVar myHandler
+	http.ListenAndServe(":8080", myVar)
+}
+
+func answer1(s string) string {
+	var reply string
+	if s == "Root Node" {
+		reply = `Correct`
+	} else if len(s) > 0 {
+		reply = `Incorrect`
+	} else {
+		reply = `You havent picked an answer`
+	}
+	return reply
+}
